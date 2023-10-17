06bd605c1825ac67398570e0d81c782b7cb36416
Alvaro Gallego <rovala18@hotmail.com>
2023-10-17 14:18:08 +0200

    Movies End Proyect

--- app/src/main/java/com/agalobr/ex03views/features/ex02movie/presentation/Ex02MovieActivityMain.kt
+++ app/src/main/java/com/agalobr/ex03views/features/ex02movie/presentation/Ex02MovieActivityMain.kt
@@ -1,17 +1,14 @@
 package com.agalobr.ex03views.features.ex02movie.presentation
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.agalobr.ex03views.R
 import com.agalobr.ex03views.databinding.ActivityEx02moviesBinding
 import com.agalobr.ex03views.features.ex02movie.data.MoviesDataRepository
 import com.agalobr.ex03views.features.ex02movie.data.local.XmlMoviesLocalDataSource
 import com.agalobr.ex03views.features.ex02movie.data.remote.ApiMoviesRemoteSource
 import com.agalobr.ex03views.features.ex02movie.domain.GetMoviesUseCase
-import com.agalobr.ex03views.features.ex02movie.domain.Movie
 import com.agalobr.ex03views.features.ex02movie.presentation.adapter.Ex02MoviesAdapter
 
 class Ex02MovieActivityMain : AppCompatActivity() {
@@ -45,9 +42,7 @@ class Ex02MovieActivityMain : AppCompatActivity() {
                     adapter = Ex02MoviesAdapter(it.movie)
                 }
             }
-            Log.d("@dev","Lista ${it.movie}")
         }
         viewModel.uiState.observe(this, observer)
     }
-
 }
